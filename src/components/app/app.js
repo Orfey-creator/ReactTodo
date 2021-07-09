e980cba9444782160d1538bd36ee066cd72a2e09
Orfey-creator <romanbez96@gmail.com>
2021-07-09 15:34:30 +0300

    create state and props for components; add func like and important

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,53 @@
-import React from 'react';
-import './app.css';
-import '../../index.css';
-import '../app-header/app-header.css';
-import '../post-add-form/post-add-form.css';
-import '../post-list/post-list.css';
-import '../post-list-item/post-list-item.css';
-import '../post-status-filter/post-status-filter.css';
-import '../search-panel/search-panel.css';
-import AppHeader from '../app-header/app-header';
-import SearchPanel from '../search-panel/search-panel';
-import PostStatusFilter from '../post-status-filter/post-status-filter';
-import PostList from '../post-list/post-list';
-import PostAddForm from '../post-add-form/post-add-form';
-
+import React from "react";
+import "./app.css";
+import "../../index.css";
+import "../app-header/app-header.css";
+import "../post-add-form/post-add-form.css";
+import "../post-list/post-list.css";
+import "../post-list-item/post-list-item.css";
+import "../post-status-filter/post-status-filter.css";
+import "../search-panel/search-panel.css";
+import AppHeader from "../app-header/app-header";
+import SearchPanel from "../search-panel/search-panel";
+import PostStatusFilter from "../post-status-filter/post-status-filter";
+import PostList from "../post-list/post-list";
+import PostAddForm from "../post-add-form/post-add-form";
 
 const App = () => {
+  const data = [
+    1,
+    2,
+    {},
+    [],
+
+    {
+      label: "Going tovpukvpuk learn React",
+      id: "sdawqd",
+    },
+    {
+      label: "Going to learn React",
+      id: "sdwqd",
+    },
+    {
+      label: "Thats was good",
+      id: "daqwdq",
+    },
+    {
+      label: "I need a break...",
+      id: "fwfa",
+    },
+  ];
   return (
     <div className="app">
-      <AppHeader/>
+      <AppHeader />
       <div className="search-panel d-flex">
-        <SearchPanel/>
-        <PostStatusFilter/>
+        <SearchPanel />
+        <PostStatusFilter />
       </div>
-      <PostList/>
-      <PostAddForm/>
+      <PostList posts={data} />
+      <PostAddForm />
     </div>
-  )
+  );
 };
 
 export default App;
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React from "react";
 
-const PostListItem = () => {
+export default class PostListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      important: false,
+      like: false,
+    };
+    this.onImportant = this.onImportant.bind(this);
+    this.onLike = this.onLike.bind(this);
+  }
+
+  onImportant() {
+    this.setState(({ important }) => ({
+      important: !important,
+    }));
+  }
+  onLike() {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  }
+
+  render() {
+    const { label } = this.props;
+    const { important, like } = this.state;
     const date = new Date();
+    let classNames = "app-list-item d-flex justify-content-between";
+    if (important) {
+      classNames += " important";
+    }
+    if (like) {
+        classNames += " like";
+    }
     return (
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hello world!
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <span>{ date.getDate() }.{ date.getMonth() }.{ date.getFullYear() }</span>
-                <button className="btn-star btn-sm">
-                    <i className="fa fa-star"></i>
-                </button>
-                <button className="btn-trash btn-sm">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-                </button>
-                <i className="fa fa-heart"></i>
-            </div>
-            
-        </li>
-    )
+      <div className={classNames}>
+        <span onClick={this.onLike} className="app-list-item-label">{label}</span>
+        <div className="d-flex justify-content-center align-items-center">
+          <span>
+            {date.getDate()}.{date.getMonth()}.{date.getFullYear()}
+          </span>
+          <button onClick={this.onImportant} className="btn-star btn-sm">
+            <i className="fa fa-star"></i>
+          </button>
+          <button className="btn-trash btn-sm">
+            <i className="fa fa-trash" aria-hidden="true"></i>
+          </button>
+          <i className="fa fa-heart"></i>
+        </div>
+      </div>
+    );
+  }
 }
-export default PostListItem;
