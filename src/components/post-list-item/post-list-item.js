9fcd03cdc5ee51d80269569e4235af8b850c49b6
Orfey-creator <romanbez96@gmail.com>
2021-07-09 13:21:30 +0300

    create gitinore, add date

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const PostListItem = () => {
+    const date = new Date();
     return (
         <li className="app-list-item d-flex justify-content-between">
             <span className="app-list-item-label">
                 Hello world!
             </span>
             <div className="d-flex justify-content-center align-items-center">
+                <span>{ date.getDate() }.{ date.getMonth() }.{ date.getFullYear() }</span>
                 <button className="btn-star btn-sm">
                     <i className="fa fa-star"></i>
                 </button>
