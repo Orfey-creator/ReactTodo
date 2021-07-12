c077bbb177baf7e999900a829e1b73b5507b4b90
Orfey-creator <romanbez96@gmail.com>
2021-07-12 16:00:35 +0300

    add: delete half add post

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,41 +13,85 @@ import PostStatusFilter from "../post-status-filter/post-status-filter";
 import PostList from "../post-list/post-list";
 import PostAddForm from "../post-add-form/post-add-form";
 
-const App = () => {
-  const data = [
-    1,
-    2,
-    {},
-    [],
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      data : [
+        1,
+        2,
+        {},
+        [],
+        {
+          label: "Going tovpukvpuk learn React",
+          id: "1",
+        },
+        {
+          label: "Going to learn React",
+          id: "2",
+        },
+        {
+          label: "Thats was good",
+          id: "3",
+        },
+        {
+          label: "I need a break...",
+          id: "4",
+        },
+      ]
+    }
+    this.deleteItem = this.deleteItem.bind(this);
+    this.addItem = this.addItem.bind(this);
 
-    {
-      label: "Going tovpukvpuk learn React",
-      id: "sdawqd",
-    },
-    {
-      label: "Going to learn React",
-      id: "sdwqd",
-    },
-    {
-      label: "Thats was good",
-      id: "daqwdq",
-    },
-    {
-      label: "I need a break...",
-      id: "fwfa",
-    },
-  ];
-  return (
-    <div className="app">
-      <AppHeader />
-      <div className="search-panel d-flex">
-        <SearchPanel />
-        <PostStatusFilter />
+    this.maxId = 5;
+  };
+
+  deleteItem (id) {
+    this.setState(({data}) => {
+      const index = data.findIndex(elem => elem.id === id );
+
+      const before = data.slice(0, index);
+      const after = data.slice(index + 1);
+
+      const newArr = [...before, ...after];
+      return {
+        data : newArr
+      }
+    });
+  }
+
+  addItem ( body ) {
+    const newItem = {
+      label : body, 
+      important : false,
+      id : this.maxId++
+    }
+    this.setState(({data}) => {
+      const newArr = [...data, newItem];
+      return {
+        data : newArr
+      }
+    })
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <AppHeader />
+        <div className="search-panel d-flex">
+          <SearchPanel />
+          <PostStatusFilter />
+        </div>
+        <PostList 
+          posts={this.state.data} 
+          onDelete = { this.deleteItem }
+        />
+        <PostAddForm 
+          onAdd = {this.addItem}
+        />
       </div>
-      <PostList posts={data} />
-      <PostAddForm />
-    </div>
-  );
+    );
+  }
 };
 
-export default App;
+
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PostListItem from '../post-list-item/post-list-item';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
     const elements = posts.map((item) => {
         if (item.id && item.label) {
             return (
                 <li key = {item.id} className="list-group-item">
-                    <PostListItem { ...item }/>
+                    <PostListItem 
+                        { ...item }
+                        onDelete = { () => onDelete(item.id)}
+                    />
                 </li>
             )
         }
